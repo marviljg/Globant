32feac362c7038a3643ee83e7ee76fcd70cf25a3
Asus Lap <marvil.jg@gmail.com>
2023-05-27 22:11:44 -0600

    Modificaciones a ejercicio 10; normales

--- g7_javaIntro/g7_myProjects/ejer7_normales/g1_e10.java
+++ g7_javaIntro/g7_myProjects/ejer7_normales/g1_e10.java
@@ -2,68 +2,61 @@ import java.util.Scanner;
 
 public class g1_e10 {
     
+    public static Scanner entrada = new Scanner(System.in);
+    public static boolean salir=false;
+    public static String confirmacion="";
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        int lection, a, b;
-        float r;
-        boolean triger=true;
-        String confirmar="";
-        System.out.println("Ingresa 2 numeros: ");
-        a = entrada.nextInt();
-        b = entrada.nextInt();
+        
+        System.out.println("¡Bienvenido!");
 
-        do{
-            System.out.println("¿Que operacion desea realizar?");
-            lection = Menu();
+        System.out.println("Ingresa el 1er número: ");
+        double n1 = entrada.nextDouble();
 
-            if (lection==5){
-                System.out.println("¿Está seguro que desea salir del programa (S/N)?: ");
-                confirmar=confirmar.toUpperCase();
-                if (confirmar.equals("S")){
-                    triger=false;
-                }
-            }
+        System.out.println("Ingresa el 2do número: ");
+        double n2 = entrada.nextDouble();
 
-            r=Calculadora(a, b, lection);
-            System.out.println("Resultado: "+r);
-            System.out.println("\n");
-        } while (triger==true);
+        Calculadora(n1, n2);
         System.out.println("Bye!");
     }
 
-    public static int Menu(){
-        Scanner entrada = new Scanner(System.in);
+    public static void Calculadora(double x, double y){
+        int op=0;
 
-        System.out.println("MENU");
-        System.out.println("1. Sumar");
-        System.out.println("2. Restar");
-        System.out.println("3. Multiplicar");
-        System.out.println("4. Dividr");
-        System.out.println("5. Salir");
-        System.out.println("Elija opción");
+        while(!salir){
+            System.out.println("MENU");
+            System.out.println("1. Sumar");
+            System.out.println("2. Restar");
+            System.out.println("3. Multiplicar");
+            System.out.println("4 Dividor");
+            System.out.println("5. Salir");
 
-        int op=entrada.nextInt();
-        return op;
-    }
+            System.out.println("Ingresa opcion: ");
+            op=entrada.nextInt();
 
-    public static float Calculadora(int x, int y, int op){
-        float result=0f;
-
-        switch (op) {
-            case 1:
-            result=x+y;
-            break;
-            case 2:
-            result=x-y;
-            break;
-            case 3:
-            result=x*y;
-            break;
-            case 4:
-            result=x/y;
-            break;
+            switch(op){
+                case 1:
+                    System.out.println("Suma es: "+(x+y));
+                    break;
+                case 2:
+                    System.out.println("Resta es: "+(x-y));
+                    break;
+                case 3:
+                    System.out.println("Multiplicación es: "+(x*y));
+                    break;
+                case 4:
+                    System.out.println("Dicisión es: "+(x/y));
+                    break;
+                case 5:
+                    System.out.println("¿Esta seguro de que desea salir? S/N: ");
+                    confirmacion=entrada.nextLine();
+                    if (confirmacion.equalsIgnoreCase("s")) {
+                        salir=true;
+                    }
+                    break;
+                default:
+                    System.out.println("Opcion invalida");
+                    break;
+            }
         }
-
-        return result;
     }
 }
