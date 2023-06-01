f484ffb5a6dcfd8ccb1f3ea05fe6850669a7104a
marvil.jg@gmail.com <marviljg>
2023-06-01 17:13:55 -0600

    Prueba de vinculacion PCG

---
+++ g7_javaIntro/g7_myProjects/ejer7_extra/ge_e2.java
@@ -0,0 +1,9 @@
+package ejer7_extra;
+
+public class ge_e2 {
+
+    public static void main(String[] args) {
+        System.out.println("Prueba");
+        System.out.println("Creo todo bien");
+    }
+}
